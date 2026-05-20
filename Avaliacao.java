330348ca13bf5300cb02527136e8213a05fe9275
luan-libarino-santos <luanlibarino@hotmail.com>
2026-05-20 22:03:58 +0000

    feito

--- Avaliacao.java
+++ Avaliacao.java
@@ -3,5 +3,35 @@
     Boa prova! */
 
 void main() {
-    IO.println("Boa Prova!");
+    double consumo = 0;
+    int dias = 1;
+    boolean continuar = true;
+    double aux;
+    double media;
+
+    while (continuar) {
+        aux = Float.parseFloat(IO.readln(("Digite a agua consumida (em Litros) no dia "+dias + ": ")));
+        consumo += aux;
+        if (aux == 0) {
+            continuar = false;
+            IO.println("Finalizando contagem...");
+            if (consumo == 0) {
+                IO.println("Nenhum consumo registrado!");
+            }
+            else {
+                media = consumo / (dias-1);
+
+                if (media < 100) {
+                    System.out.printf("Média de %.2f litros por dia: Consumo Sustentável!", media);
+                }
+                if (media >= 100 & media<= 175) {
+                    System.out.printf("Média de %.2f litros por dia: Consumo Moderado!", media);
+                }
+                if (media > 175) {
+                    System.out.printf("Média de %.2f litros por dia: Consumo Excessivo!", media);
+                }
+            }
+    }
+        dias +=1;
+}
 }
